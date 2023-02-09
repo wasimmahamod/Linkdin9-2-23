41cc010c47b79525fe5e3a525140c717f67e5741
wasimmahamod <95435314+wasimmahamod@users.noreply.github.com>
2023-02-09 20:53:50 +0600

    first commit

---
+++ src/compnents/Container.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Container = ({children}) => {
+  return (
+    <div className='max-w-container mx-auto px-5 '>{children}</div>
+  )
+}
+
+export default Container
---
+++ src/compnents/ContainerL.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ContainerL = ({children}) => {
+  return (
+    <div className='max-w-containerfluid mx-auto px-3'>{children}</div>
+  )
+}
+
+export default ContainerL
